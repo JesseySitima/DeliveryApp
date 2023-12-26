a3891870256be20ad5a04d80d5129a8506fd0493
unknown <jaysitima@gmail.com>
2023-12-26 21:15:23 +0200

    cart changes

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View, Image, Button } from 'react-native';
-import { useSelector } from 'react-redux';
+import { TouchableOpacity, StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
+import { addDish, removeDish, selectSelectedDishes } from '../features/basketSlice';
 
 const CartScreen = ({ onPress }) => {
   const basket = useSelector((state) => state.basket);
+  const dispatch = useDispatch();
   const [totalPrice, setTotalPrice] = useState(0);
   const navigation = useNavigation();
+  const dishes = useSelector((state) => state.dishes);
+
+
 
   // Function to get the array of items in the basket
   const getBasketItems = () => {
@@ -31,11 +36,18 @@ const CartScreen = ({ onPress }) => {
   const handleOrder = () => {
     navigation.navigate("preparOrder")
   };
+  const handleAddToCart = (dishId) => {
+    
+  };
+  
+  const handleRemoveFromCart = (dishId) => {
+   
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Items in Cart:</Text>
-      <View>
+      <ScrollView style={styles.scrollView}>
         {Object.keys(basket.selectedDishes).map((dishId, index) => {
           const item = basket.selectedDishes[dishId];
           const totalItemPrice = item.quantity * item.price;
@@ -43,33 +55,39 @@ const CartScreen = ({ onPress }) => {
             <View style={styles.itemContainer} key={index}>
               <Image source={item.imageUrl} style={styles.image} />
               <View style={styles.itemInfo}>
-                <Text style={styles.itemId}>Item ID: {dishId}</Text>
-                <Text style={styles.itemName}>Name: {item.name}</Text>
-                <Text style={styles.itemPrice}>Price: ${item.price}</Text>
+               
+                <Text style={styles.itemName}>{item.name}</Text>
+                <Text style={styles.itemPrice}>{item.quantity} X MWK{item.price}</Text>
                 <Text style={styles.itemPrice}>
-                  Quantity: {item.quantity}
-                </Text>
-                <Text style={styles.itemPrice}>
-                  Total Price: ${totalItemPrice}
+                  Total: MWK{totalItemPrice}
                 </Text>
               </View>
+              <View style={styles.quantityContainer}>
+          <TouchableOpacity onPress={() => handleRemoveFromCart(dishId)}>
+            <Text style={styles.quantityButton}>-</Text>
+          </TouchableOpacity>
+          <Text style={styles.quantity}>{item.quantity}</Text>
+          <TouchableOpacity onPress={() => handleAddToCart(dishId)}>
+            <Text style={styles.quantityButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+
             </View>
           );
         })}
-        <Text style={styles.totalText}>Total Price: ${getTotalPrice()}</Text>
-        <Button
-          title="Order"
-          onPress={handleOrder}
-          style={styles.orderButton}
-          textStyle={styles.orderButtonText}
-        />
-      </View>
+      </ScrollView>
+      <Text style={styles.totalText}>Total Price: MWK{getTotalPrice()}</Text>
+      <Button
+        title="Order"
+        onPress={handleOrder}
+        style={styles.orderButton}
+        textStyle={styles.orderButtonText}
+      />
     </View>
   );
 };
 
 export default CartScreen;
-
 
 const styles = StyleSheet.create({
   container: {
@@ -81,6 +99,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
+  },
+  scrollView: {
+    flex: 1,
+    marginBottom: 20,
   },
   itemContainer: {
     flexDirection: 'row',
@@ -129,6 +151,24 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     padding: 10,
+  },
+  quantityContainer: {
+    
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  quantityButton: {
+    fontSize: 20,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+  },
+  quantity: {
+    fontSize: 16,
+    marginHorizontal: 10,
+    paddingTop: 5,
+    paddingBottom: 5
   },
 });
 
